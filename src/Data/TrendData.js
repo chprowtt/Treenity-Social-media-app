ca8d40cd128f0cfc24b5f77f1656472234593f36
Ghali sajid <ghali.sajid96@gmail.com>
2022-09-02 18:34:20 +0200

    first commit

---
+++ src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "Elonmusk",
+      shares: 85,
+    },
+    {
+      name: "TristianTate",
+      shares: 83.5,
+    },
+    {
+      name: "Zainkeepscode",
+      shares: 75.5,
+    },
+    {
+      name: "Reactjs",
+      shares: 72,
+    },
+    {
+      name: "Elon Musk",
+      shares: 71.9,
+    },
+    {
+      name: "Need for Speed",
+      shares: 20,
+    },
+  ];
+  
